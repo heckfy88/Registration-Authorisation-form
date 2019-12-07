9e4d4b2c85e2dc94a9515ccc0d85a842db2815d9
heckfy88 <54860448+heckfy88@users.noreply.github.com>
2019-12-07 23:37:33 +0300

    Update authorisation_form.php

--- Registration:Authorisation form/authorisation_form.php
+++ Registration:Authorisation form/authorisation_form.php
@@ -2,7 +2,7 @@
 // Initialize the session
 session_start();
 
-// Check if the user is already logged in, if yes then redirect him to welcome page
+// Check if the user is already logged in, if true then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: welcome.php");
     exit;
